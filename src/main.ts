785b22dafd9444e16cf33998597a7af753e03bc9
Samuel Finocchio <samuelfinocchioit@gmail.com>
2023-09-16 20:38:59 +0200

    Fixed package json

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { piston } from './piston';
-
-(async () => {
-    const pistonInstance = await piston("http://localhost:2000");
-
-    console.log(pistonInstance.configuration);
-    console.log(await pistonInstance.getRuntimes());
-    console.log(await pistonInstance.executeSourceCode("r = input()\nprint('Hello ' + r)", "python", "Bosica"));
-})();
